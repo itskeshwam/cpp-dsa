48269ab0f7a5b4d6f2571ed268d500a78f864dcd
Keshwam Pandey <pandeykeshwam1122@gmail.com>
2025-05-20 11:25:09 +0530

    Data Types

--- demo.cpp
+++ demo.cpp
@@ -1,8 +1,45 @@
-#include<iostream>
-using namespace std;
+#include <bits/stdc++.h>
+using namespace std; // for not using std:: prefix
+// Input and Output
+
+// int main() {
+//     int x, y;
+//     cin >> x >> y;
+//     cout << "value of x: " << x << " and y: " << y; 
+//     return 0;
+// }
+
+
+// Data Types 
 int main() {
-    int x, y;
-    cin >> x >> y;
-    cout << "value of x: " << x << " and y: " << y; 
+    // // int (integer)
+    // int x = 10; 
+
+    // // long (for long integers)
+    // long y = 1000000000;
+    
+    // //long long (for very long integers)
+    // long long z = 1000000000000000;
+    
+    // // float (for floating point numbers)
+    // float z = 10.5;
+    
+    // // double (for double precision floating point numbers)
+    // double a = 10.5;
+    
+    // // string
+    // string s1, s2;
+    // cin >> s1 >> s2; // reads a word
+    // cout << s1 << " " << s2 << endl; // used 2 variable s1 and s2 cuz space is not allowed in string so "hey brother" needs 2 variables s1 and s2 respectively
+
+    // // getline (takes entire line and not just a word)
+    // string s3;
+    // getline(cin, s3); 
+    // cout << s3;
+
+    //char (used for single character)
+    char c = 'a';
+    cout << c << endl;
+
     return 0;
 }
