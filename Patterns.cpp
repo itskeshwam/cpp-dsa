46f81f3a7a8098d618812bb51f7c83207cd8c5b6
Keshwam Pandey <pandeykeshwam1122@gmail.com>
2025-05-26 21:18:40 +0530

    Left triangle pattern

--- Patterns.cpp
+++ Patterns.cpp
@@ -13,3 +13,26 @@ using namespace std;
 //     }
 // }
 
+// left triangle star pattern
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     for (int i=1; i<=n; i++){
+//         for(int j=1; j<=i; j++){
+//             cout << '*';
+//         } cout << endl;
+//     } 
+// } 
+
+// 1/12/123/1234/12345 pattern in left triangle 
+int main(){
+    int n;
+    cin >> n;
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=i; j++){
+            cout << j;
+        } cout << endl;
+    }
+
+}
