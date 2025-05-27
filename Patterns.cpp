20e20e2107f4ebf3f701c1847a00fdf74bf23a90
Keshwam Pandey <pandeykeshwam1122@gmail.com>
2025-05-27 13:16:13 +0530

    Triangle Pattern

--- Patterns.cpp
+++ Patterns.cpp
@@ -137,16 +137,30 @@ using namespace std;
 
 // rotated triangle 
 // right half diamond pattern inside
+// int main(){
+//     int n;
+//     cin >> n;
+//     for (int i=1; i<=2*n-1;i++){
+//         int stars = i;
+//         if (i>n) stars = 2*n-i;
+//         for (int j=1; j<=stars; j++){
+//             cout << "*";
+//         }cout << endl;
+//     }
+// }
+
+// binary triangle 
 int main(){
     int n;
     cin >> n;
-    for(int i=1;i<=2*n-1;i++){
-        int stars = i;
-        
-        if(i>n) stars = 2*n-i;
-        
-        for(int j=1;j<=stars;j++){
-            cout<<"*";
-        }cout << endl;
+    int start = 1;
+    for (int i=0;i<n;i++){
+        if (i%2==0) start = 1;
+        else start = 0;
+        for (int j=0;j<=i;j++){
+            cout << start;
+            start = 1- start;
+        }
+        cout << endl;
     }
 }
