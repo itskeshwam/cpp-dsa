221b1ee6c78b8fddf7477663643481749224fda9
Keshwam Pandey <pandeykeshwam1122@gmail.com>
2025-05-28 15:01:36 +0530

    Alphabet Patterns

--- Patterns.cpp
+++ Patterns.cpp
@@ -36,6 +36,17 @@ using namespace std;
 //     }
 // }
 
+// row number triangle
+// int main(){
+//     int n;
+//     cin >> n;
+//     for (int i=1; i<=n; i++){
+//         for (int j=1; j<=i; j++){
+//             cout << i;
+//         } cout << endl;
+//     }
+// }
+
 
 // inverted left triangle 
 // int main(){
@@ -225,20 +236,69 @@ using namespace std;
 //     for (int i=1; i<=n; i++){
 //         for (int j=1;j<=i;j++){
 //             cout << x << " ";
-//             x+=1;
+//             x++;
 //         }cout << endl;
 //     }
 // } 
 
 // decreasing letter pattern
+// int main(){
+//     int n;
+//     cin >> n;
+//     char x = 'A';
+//     for (int i=1; i<=n; i++){
+//         for (int j=n;j>=i;j--){
+//             cout << x << " ";
+//             x+=1;
+//         }cout << endl;
+//     }
+// }
+
+// decreasing letter loop
+// int main(){
+//     int n;
+//     cin >> n;
+//         for (int i=0; i<=n; i++){
+//         for(char ch = 'A'; ch<='A'+(n-i-1);ch++){
+//             cout << ch << " ";
+//         }cout << endl;
+//     }
+// }
+
+
+// increasing letter pattern
+// int main(){
+//     int n;
+//     cin >> n;
+//     for (int i=0; i<=n; i++){
+//         for(char ch = 'A'; ch<='A'+i;ch++){
+//             cout << ch << " ";
+//         }cout << endl;
+//     }
+// }
+
+
+// Alpha Hill pattern
 int main(){
     int n;
     cin >> n;
-    char x = 'A';
-    for (int i=1; i<=n; i++){
-        for (int j=n;j>=i;j--){
-            cout << x << " ";
-            x+=1;
+    for (int i = 0; i<n; i++){
+        // spaces
+        for (int j=1;j<=n-i-1;j++){
+            cout << " ";
+        }
+
+        // characters
+        char ch = 'A';
+        int breakpoint = (2*i-1);
+        for (int j = 1; j<= (2*i+1); j++){
+            cout << ch;
+            if (j<= breakpoint) ch++;
+            else ch --;
+    }
+        // spaces
+        for (int j=1;j<=n-i-1;j++){
+            cout << " ";
         }cout << endl;
     }
-} 
+}
