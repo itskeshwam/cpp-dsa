cc26b560c5283aadb7d1ccfed120af1067ae7ea8
Keshwam Pandey <pandeykeshwam1122@gmail.com>
2025-05-29 11:29:25 +0530

    STL - Pairs

--- Patterns.cpp
+++ Patterns.cpp
@@ -279,26 +279,60 @@ using namespace std;
 
 
 // Alpha Hill pattern
+// int main(){
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i<n; i++){
+        
+//         // spaces
+//         for (int j=1;j<=n-i-1;j++){
+//             cout << " ";
+//         }
+        
+//         // characters
+//         char ch = 'A';
+//         int breakpoint = (2*i-1);
+//         for (int j = 1; j<= (2*i+1); j++){
+//             cout << ch;
+//             if (j<= breakpoint) ch++;
+//             else ch --;
+//         }
+
+//         // spaces
+//         for (int j=1;j<=n-i-1;j++){
+//             cout << " ";
+//         }cout << endl;
+//     }
+// }
+
+// last first alpha triangle
+// int main(){
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i<n; i++){
+//         for (int j=0;j<=i;j++){
+//             cout << char('A' + n-1-j) << " ";
+//         }cout << endl;
+//     }
+// }
+
+// symmetry void
 int main(){
     int n;
     cin >> n;
-    for (int i = 0; i<n; i++){
-        // spaces
-        for (int j=1;j<=n-i-1;j++){
-            cout << " ";
-        }
-
-        // characters
-        char ch = 'A';
-        int breakpoint = (2*i-1);
-        for (int j = 1; j<= (2*i+1); j++){
-            cout << ch;
-            if (j<= breakpoint) ch++;
-            else ch --;
-    }
-        // spaces
-        for (int j=1;j<=n-i-1;j++){
-            cout << " ";
-        }cout << endl;
+    for (int i = 1; i<=n; i++){
+        
     }
 }
+
+
+// symmetry 
+
+
+
+// rectangle 
+
+
+
+
+// number filled box 
--- stl.cpp
+++ stl.cpp
@@ -1,3 +1,62 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h> //includes all libraries 
+// <math.h> for math
+// <string.h> for string 
+// <iostream.h> for input output 
+using namespace std; // if not you have to write std::cin or std::cout >
 
+// functions 
+void print(){ // void function - dosn't return anything
+    cout << "raj";
+}
+int sum(int a, int b){
+    return a+b;
+}
+
+int main(){ // main function - entry point of the program
+    print();
+    int s = sum(1, 5);
+    // prints 6
+    cout << s;
+    return 0;
+}
+
+
+// stls
+// 4 types: 
+// 1) algorithms
+
+
+// 2) containers
+void explainPairs(){
+    pair<int , int> a = {1, 3}; // pair is a container that holds two values
+    
+    cout << a.first << " " << a.second << endl; // prints 1 3
+    
+    pair<int, pair<int, int>> p = {1, {2, 3}}; // nested pair
+    
+    cout << p.first << " " << p.second.first << " " << p.second.second << endl; // prints 1 2 3
+
+    pair<int, int> arr[] = { {1, 2}, {2, 5}, {5, 1} }; // array of pairs
+
+    cout << arr[1].second << endl; // prints 5
+}
+
+// 3) function objects
+
+
+// 4) iterators
+
+
+
+
+// pairs 
+
+
+
+
+// vectors 
+
+
+
+
+// 
