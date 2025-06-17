ae6c414d7ded526d980daac57168b50a93771df1
Keshwam Pandey <pandeykeshwam1122@gmail.com>
2025-06-17 20:12:42 +0530

    Start Recursion

---
+++ recursion.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// print name 5 times 
+
+
+
+
+
+//print linearly from 1 to n
+
+
+
+
+
+// print n to 1
+
+
+
+
+
+// print linearly from 1 to n using backtracking
+
+
+
+
+
+// print linearly from n to 1 using backtracking
