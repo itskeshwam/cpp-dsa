02f32de5d3ca2af81705af10dd05109954ed5532
Keshwam Pandey <pandeykeshwam1122@gmail.com>
2025-06-21 19:59:35 +0530

    Reverse an array

--- recursion.cpp
+++ recursion.cpp
@@ -102,3 +102,24 @@ using namespace std;
 //     cin >> n;
 //     cout << fact(n);
 // }
+
+
+
+// reverse an array using 2 pointers or 2 variables
+void reverse(int arr[], int i, int n){
+    if (i >= n / 2) return;
+    swap(arr[i], arr[n - i - 1]);
+    reverse(arr, i + 1, n);
+}
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    reverse(arr, 0, n);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+}
