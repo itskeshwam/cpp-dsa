73966ae4cb60bbd646bdbc20425faf552a16e4e7
Keshwam Pandey <pandeykeshwam1122@gmail.com>
2025-07-16 22:23:45 +0530

    Maximum Freq Problem

--- hashing.cpp
+++ hashing.cpp
@@ -85,28 +85,56 @@ using namespace std;
 
 
 // same for characters
+// int main() {
+//     string s;
+//     cin >> s;
+
+//     // precompute
+//     map<char, int> mpp;
+//     for (int i = 0; i < s.size(); i++) {
+//         mpp[s[i]]++;
+//     }
+
+//     // iterate through the map
+//     for (auto it : mpp) {
+//         cout << it.first << "->" << it.second << endl; // key and value
+//     }
+
+//     int q;
+//     cin >> q;
+//     while (q--) {
+//         char c;
+//         cin >> c;
+//         // fetch
+//         cout << mpp[c] << endl;   
+//     }
+//     return 0;
+// }
+
+
+
+// ALWAYS PREFER USING UNORDERED MAP FOR HASHING before using map... and if and when it shows time limit exceeded, then use map as unordered map is not ordered and it uses hashing which is faster than map 
+
+// and for unordered the O(N) would happen because of collisions in the hash table, which can lead to a worst-case time complexity of O(N) for certain operations. However, in practice, unordered_map is often faster than map due to its average-case time complexity of O(1) for lookups, insertions, and deletions.
+// In contrast, map has a time complexity of O(log N) for these operations because it is implemented as a balanced binary search tree. This means that while unordered_map can be faster on average, it can also have worse performance in cases of high collision rates, leading to longer chains of elements in the hash table.
+
+// code for maximum frequency of a character in a string
 int main() {
     string s;
     cin >> s;
-
-    // precompute
     map<char, int> mpp;
     for (int i = 0; i < s.size(); i++) {
         mpp[s[i]]++;
     }
 
-    // iterate through the map
+    char maxChar = s[0];
+    int maxFreq = mpp[maxChar];
     for (auto it : mpp) {
-        cout << it.first << "->" << it.second << endl; // key and value
+        if (it.second > maxFreq) {
+            maxFreq = it.second;
+            maxChar = it.first;
+        }
     }
-
-    int q;
-    cin >> q;
-    while (q--) {
-        char c;
-        cin >> c;
-        // fetch
-        cout << mpp[c] << endl;   
-    }
+    cout << maxChar << "->" << maxFreq << endl;
     return 0;
 }
