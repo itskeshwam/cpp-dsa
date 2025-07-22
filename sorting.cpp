1168225c9a7ddcb716f61a79c91a6b9ec27f25ed
Keshwam Pandey <pandeykeshwam1122@gmail.com>
2025-07-22 11:53:32 +0530

    Selection sort

---
+++ sorting.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// selection sort 
+void selection_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int mini = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[mini]) {
+                mini = j;
+            }
+        }
+        swap(arr[i], arr[mini]);
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {cin >> arr[i];}
+    selection_sort(arr, n);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
