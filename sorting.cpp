746e9fc53e27ac47a4fc498e4467760e8b952aa2
Keshwam Pandey <pandeykeshwam1122@gmail.com>
2025-07-22 19:03:15 +0530

    Bubble sort

--- sorting.cpp
+++ sorting.cpp
@@ -1,16 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// selection sort 
-void selection_sort(int arr[], int n) {
+// // selection sort 
+// void selection_sort(int arr[], int n) {
+//     for (int i = 0; i < n - 1; i++) {
+//         int mini = i;
+//         for (int j = i + 1; j < n; j++) {
+//             if (arr[j] < arr[mini]) {
+//                 mini = j;
+//             }
+//         }
+//         swap(arr[i], arr[mini]);
+//     }
+// }
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {cin >> arr[i];}
+//     selection_sort(arr, n);
+//     for (int i = 0; i < n; i++) {
+//         cout << arr[i] << " ";
+//     }
+//     return 0;
+// }
+
+
+// bubble sort
+void bubble_sort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
-        int mini = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[mini]) {
-                mini = j;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
             }
         }
-        swap(arr[i], arr[mini]);
     }
 }
 
@@ -19,9 +43,10 @@ int main(){
     cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++) {cin >> arr[i];}
-    selection_sort(arr, n);
+    bubble_sort(arr, n);
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
     return 0;
 }
+
