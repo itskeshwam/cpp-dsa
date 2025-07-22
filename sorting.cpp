0463e1c6270eb62f0da1382806fb35ff0db7b48e
Keshwam Pandey <pandeykeshwam1122@gmail.com>
2025-07-22 20:20:16 +0530

    Insertion sort

--- sorting.cpp
+++ sorting.cpp
@@ -1,30 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// // selection sort 
-// void selection_sort(int arr[], int n) {
-//     for (int i = 0; i < n - 1; i++) {
-//         int mini = i;
-//         for (int j = i + 1; j < n; j++) {
-//             if (arr[j] < arr[mini]) {
-//                 mini = j;
-//             }
-//         }
-//         swap(arr[i], arr[mini]);
-//     }
-// }
+// selection sort 
+void selection_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int mini = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[mini]) {
+                mini = j;
+            }
+        }
+        swap(arr[i], arr[mini]);
+    }
+}
 
-// int main(){
-//     int n;
-//     cin >> n;
-//     int arr[n];
-//     for (int i = 0; i < n; i++) {cin >> arr[i];}
-//     selection_sort(arr, n);
-//     for (int i = 0; i < n; i++) {
-//         cout << arr[i] << " ";
-//     }
-//     return 0;
-// }
 
 
 // bubble sort
@@ -38,15 +27,31 @@ void bubble_sort(int arr[], int n) {
     }
 }
 
+
+
+// insertion sort
+void insertion_sort(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        int j = i;
+        while (j > 0 && arr[j-1] > arr[j]) {
+            swap(arr[j], arr[j-1]);
+        j--;
+        }
+    }
+}
+
+
+
 int main(){
     int n;
     cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++) {cin >> arr[i];}
-    bubble_sort(arr, n);
+    // selection_sort(arr, n);
+    // bubble_sort(arr, n);
+    insertion_sort(arr, n);
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
     return 0;
 }
-
