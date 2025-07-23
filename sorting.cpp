132053e45ba1e6eedc9b48a988a4439cb293ef36
Keshwam Pandey <pandeykeshwam1122@gmail.com>
2025-07-23 21:20:30 +0530

    Merge Sort

--- sorting.cpp
+++ sorting.cpp
@@ -42,14 +42,54 @@ void insertion_sort(int arr[], int n) {
 
 
 
+// merge sort
+void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp;
+    int left = low, right = mid + 1;
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        } 
+        else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+// merge sort function
+void merge_sort(vector<int> &arr, int low, int high) {
+    if (low >= high) return;
+    // base case 👆🏻
+    int mid = low + (high - low) / 2;
+    merge_sort(arr, low, mid);
+    merge_sort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+
 int main(){
     int n;
     cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++) {cin >> arr[i];}
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {cin >> arr[i];} 
     // selection_sort(arr, n);
     // bubble_sort(arr, n);
-    insertion_sort(arr, n);
+    // insertion_sort(arr, n);
+    merge_sort(arr, 0, n - 1);
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
